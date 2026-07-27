e437daf1985584d0f4fa3e0bab7588b989aaf821
Фёдор Полевой <polevoy@ligat.su>
2026-07-27 10:28:25 +0300

    Полупрозрачный header для ПК

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     <header className={cn("fixed top-2 left-0 z-50 w-full px-4", "lg:top-4")}>
       <div
         className={cn(
-          "bg-background shadow-nav transition-width relative flex w-full max-w-full justify-between gap-1 rounded-[20px] py-1 pr-4 pl-1",
+          "bg-background/80 shadow-nav transition-width relative flex w-full max-w-full justify-between gap-1 rounded-[20px] py-1 pr-4 pl-1 backdrop-blur-xl backdrop-saturate-150",
           "lg:mx-auto lg:w-fit lg:min-w-3xl lg:p-1.5 xl:gap-1.5",
         )}
       >
