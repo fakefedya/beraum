6c7a1d5e7dc55ec1dc1e6958dd963339c3c8abe2
Фёдор Полевой <polevoy@ligat.su>
2026-08-26 11:35:37 +0300

    Очистка от мусора

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     dangerouslyAllowLocalIP: true,
-//     qualities: [75, 95],
-//     formats: ["image/avif", "image/webp"],
-
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "9000",
-//         pathname: "/system-assets/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "9000",
-//         pathname: "/products/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const storageUrlStr =
